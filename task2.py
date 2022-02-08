5fe84617c21cc0360bbd59d0b6fbc59fc21360fb
magdychserg <magdych@mail.ru>
2022-02-08 23:36:57 +1200

    lesson2

---
+++ task2.py
@@ -0,0 +1,35 @@
+'''2. Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON с информацией о заказах. Написать
+скрипт, автоматизирующий его заполнение данными. Для этого:
+
+Создать функцию write_order_to_json(), в которую передается 5 параметров — товар (item), количество (quantity),
+цена (price), покупатель (buyer), дата (date). Функция должна предусматривать запись данных в виде словаря в файл
+orders.json. При записи данных указать величину отступа в 4 пробельных символа;
+Проверить работу программы через вызов функции write_order_to_json() с передачей в нее значений каждого параметра.'''
+
+import os
+import json
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    filename = os.path.join(CURRENT_DIR, 'source_data\orders.json')
+
+    if os.path.exists(filename):
+        with open(filename, encoding="utf-8") as fl:
+            data = json.loads(fl.read())
+
+        data['orders'].append({'item': item, 'quantity': quantity, 'price': price, 'buyer': buyer, 'date': date})
+
+        with open(filename, "w", encoding="utf-8") as fl:
+            json.dump(data, fl, indent=4, separators=(',', ': '), ensure_ascii=False)
+
+        print(f'Data added to {filename}')
+
+    else:
+        print(f'Source file path {filename} not found')
+
+
+if __name__ == '__main__':
+    write_order_to_json('LENOVO', '3', '7000', 'Сидоров', '08.02.2022')
+    write_order_to_json('ACER', '2', '4500', 'Сидоров', '08.02.2022')
