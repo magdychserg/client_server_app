5c7f705f454da87f7ecb6ad4d2aadc38a81a103e
magdychserg <magdych@mail.ru>
2022-03-26 09:37:41 +1200

    lesson1 with OOP

---
+++ launcher.py
@@ -0,0 +1,25 @@
+import subprocess
+
+PROCESS = []
+
+while True:
+    ACTION = input('Выберите действие: q - выход, '
+                   's - запустить сервер и клиенты, x - закрыть все окна: ')
+
+    if ACTION == 'q':
+        break
+    elif ACTION == 's':
+        PROCESS.append(subprocess.Popen('python server.py',
+                                        creationflags=subprocess.CREATE_NEW_CONSOLE))
+
+        PROCESS.append(subprocess.Popen('python client.py -n user1',
+                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
+
+        PROCESS.append(subprocess.Popen('python client.py -n user2',
+                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESS.append(subprocess.Popen('python client.py -n user3',
+                                        creationflags=subprocess.CREATE_NEW_CONSOLE))
+    elif ACTION == 'x':
+        while PROCESS:
+            VICTIM = PROCESS.pop()
+            VICTIM.kill()
